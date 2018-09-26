e269546128b39b7e5533674ad9fa563f86d8a24d
Lelya <Kovyrshina.olga@yandex.ru>
2018-09-27 02:36:12 +0300

    update

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,44 @@
 <?php
-
+/**
+ * Подключаем файл
+ */
 include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
+/**
+ * Инициализация объекта класса Init
+ */
+$init = new App\Acme\Init();
 
 ?><!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
+        <title>Test</title>
     </head>
     <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
+        <h4>Выборка из таблицы test данных по критерию: result среди значений 'normal' и'success';</h4>
+        <?php
+
+        $result = $init->get();
+
+        /**
+         * Вывод на страничку в виде таблицы
+         */
+        echo "<table border=1>
+                <tr><th>id</th><th>script_name</th><th>sort_index</th><th>start_time</th><th>result</th></tr>";
+
+                //вывод построчно
+                for($i = 0; $i < count($result); $i++){
+                        echo
+                        "<tr>
+                                <td>".$result[$i]['ID']."</td>
+                                <td>".$result[$i]['script_name']."</td>
+                                <td>".$result[$i]['sort_index']."</td>
+                                <td>".$result[$i]['start_time']."</td>
+                                <td>".$result[$i]['result']."</td>
+                        </tr>";
+                }
+                        echo "</table>";
+
+        ?>
     </body>
 </html>
