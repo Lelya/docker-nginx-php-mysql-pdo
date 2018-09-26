cae708f715ca1651c4bc05ec2133df01a7c24dc3
Lelya <Kovyrshina.olga@yandex.ru>
2018-09-27 02:37:50 +0300

    update

--- web/app/src/Init.php
+++ web/app/src/Init.php
@@ -1,44 +1,111 @@
 <?php
+/**
+ * Файл класса Init
+ *
+ * @author Lelya <kovyrshina.olga@yandex.ru>
+ * @version 1.0
+ */
 
 namespace App\Acme;
+use PDO,PDOException;
 
-
+/**
+ * Класс Init
+ * от которого нельзя сделать наследника
+ */
 final class Init
 {
-        private $host = "localhost";
+        /**
+         * @var string адрес сервера mysql
+         */
+        private $host = "mysql";
+        /**
+         * @var string имя пользователя
+         */
         private $user = "dev";
+        /**
+         * @var string пароль для пользователя
+         */
         private $pass = "dev";
+        /**
+         * @var string имя базы данных
+         */
         private $database = "test";
-        private $dbcharset = "utf-8";
-        public $dbh;
+        /**
+         * @var \PDO соединение с базой
+         */
+        private $connection;
 
-        public function create()
-        {
-                $dsn = "mysql:host=$this->host;dbname=$this->database;charset=$this->dbcharset";
-                $opt = [
-                        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                        PDO::ATTR_EMULATE_PREPARES   => false,
-                ];
 
+        function __construct() {
                 try {
-                        $pdo = new PDO($dsn, $this->user, $this->pass, $opt);
-                        return 'MySQL PDO';
+                        $this->connection = new PDO("mysql:host={$this->host};dbname={$this->database};", $this->user, $this->pass);
+                        $this->connection->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 }
                 catch (PDOException $e) {
-                        echo '<br /><p align="center"><strong>Connect error:</strong> '.$e->getMessage().'</p>';
-                        exit();
-//                        die('Подключение не удалось: ' . $e->getMessage());
+                        die('Подключение не удалось: ' . $e->getMessage());
+                }
+                $this->create();
+                $this->fill();
+        }
+
+        /**
+         * Создает таблицу test, содержащую 5 полей
+         *   - id целое, автоинкрементарное;
+         *   - script_name строковое, длиной 25 символов;
+         *   - start_time timestamp с автозаполнением;
+         *   - sort_index целое (значения не превышают 3-х разрядов);
+         *   - result один вариант из 'normal', 'illegal', 'failed', 'success';
+         */
+        private function create()
+        {
+                //запрос на создание таблицы
+                $query = $this->connection->prepare("CREATE TABLE IF NOT EXISTS `test` (
+                        `ID` INT NOT NULL AUTO_INCREMENT,
+                        `script_name` varchar(25),
+                        `start_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        `sort_index` INT,
+                        `result` ENUM('normal', 'illegal', 'failed', 'success'),
+                        PRIMARY KEY(`ID`))");
+                $query->execute();
+
+        }
+
+        /**
+         * Заполняет таблицу test случайными данными;
+         */
+        private function fill()
+        {
+                $array_result = array("normal", "success", "illegal", "failed");
+
+                for ($i = 1; $i <= 50; $i++) {
+
+                        $rand_result = array_rand($array_result, 2);
+                        $rand_scriptname = substr(md5(microtime()),rand(0,26),10);
+                        $rand_sortindex = rand(0, 999);
+
+                        $statement = $this->connection->prepare('INSERT INTO test (script_name, sort_index, result)
+                              VALUES (:script_name, :sort_index, :result)');
+
+                        $statement->execute([
+                                'script_name' => $rand_scriptname,
+                                'sort_index' => $rand_sortindex,
+                                'result' => $array_result[$rand_result[0]]
+                        ]);
                 }
         }
 
-//        private function fill()
-//        {
-//                return 'Nginx PHP MySQL';
-//        }
-//
-//        public function get()
-//        {
-//                return 'Nginx PHP MySQL';
-//        }
+        /**
+         * Выбирает из таблицы test, данные по критерию: result среди значений 'normal' и 'success';
+         * @return array массив значений
+         */
+        public function get()
+        {
+                $statement = $this->connection->prepare("SELECT * FROM test WHERE result IN ('normal','success')");
+                $statement->execute();
+
+                $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+                return $data;
+        }
+
 }
